88e27a056bd156e6edd4533f3e604b2fb0b0df7d
Joshua T Corbin <jcorbin@wunjo.org>
2023-05-16 16:09:02 -0400

    Add some riser baseplate models

---
+++ base_models/generate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd $(dirname "$0")
+
+width=3
+depth=3
+height=4
+
+for width in 3 4 5 6; do
+  for depth in 3 4 5; do
+    [ $depth -gt $width ] && continue
+    openscad \
+      ../gridfinity_riser.scad \
+      -D base=true \
+      -D base_clips=true \
+      -D width=${width} \
+      -D depth=${depth} \
+      -D height=${height} \
+      -o tunnel_base_${width}x${depth}x${height}_with_clip.stl
+  done
+done
