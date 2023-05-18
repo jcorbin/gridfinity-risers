797137c48e388e6c9032e833d38c0634c4fdd36f
Joshua T Corbin <jcorbin@wunjo.org>
2023-05-18 12:52:26 -0400

    Add some 1u grid base models

--- base_models/generate.sh
+++ base_models/generate.sh
@@ -21,3 +21,19 @@ for width in 3 4 5 6; do
       -o tunnel_base_${width}x${depth}x${height}_with_clip.stl
   done
 done
+
+height=1
+
+for width in 3 4 5 6; do
+  for depth in 3 4 5; do
+    [ $depth -gt $width ] && continue
+    openscad \
+      ../gridfinity_riser.scad \
+      -D base=true \
+      -D base_clips=true \
+      -D width=${width} \
+      -D depth=${depth} \
+      -D height=${height} \
+      -o grid_base_${width}x${depth}_with_clip.stl
+  done
+done
